d2c73c669281ba24d659bf85fe410d189c2460d2
Mehra <mehra9@avaya.com>
2020-05-12 22:23:56 +0530

    New app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Login from './Screens/login'
+import { HashRouter, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+
+  render() {
+
+    const { history } = this.props;
+    return (
+     
+        <HashRouter>
+          <div class="parent">          
+          <div class="navHeader ">
+ 
+            </div>
+
+            <div class="wrapper" style={{overflowY: "auto"}}>
+              <Route exact path='/Login' component={Login} />
+              
+            </div>
+
+
+          </div>
+        </HashRouter>
+  
+    );
+  }
 }
 
 export default App;
---
+++ src/Screens/login.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+class Login extends Component {
+
+
+    render(){
+
+        return (
+
+            <div>
+                <p>Music is Life!!!</p>
+            </div>
+        );
+    }
+
+}
+
+
+export default Login;
